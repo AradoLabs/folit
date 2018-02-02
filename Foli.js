a8a9e914c0f0fc0fd6918a34770582b6cb202a63
Mikko Junnila <mikko.junnila@gmail.com>
2018-02-02 15:00:04 +0200

    Animate them föli

--- Foli.js
+++ Foli.js
@@ -1,11 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { MapView } from "expo";
 import { Ionicons } from "@expo/vector-icons";
 
 export default class Foli extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      coordinate: new MapView.AnimatedRegion({
+        latitude: 0,
+        longitude: 0,
+      }),
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const duration = 1500;
+    const nextCoordinate = {
+      latitude: nextProps.latitude,
+      longitude: nextProps.longitude,
+    };
+
+    const propsMatch =
+      this.props.latitude === nextProps.latitude &&
+      this.props.longitude === nextProps.longitude;
+
+    if (!propsMatch) {
+      if (Platform.OS === "android") {
+        if (this.marker) {
+          this.marker._component.animateMarkerToCoordinate(
+            nextCoordinate,
+            duration,
+          );
+        }
+      } else {
+        this.state.coordinate
+          .timing({
+            ...nextCoordinate,
+            duration,
+            useNativeDriver: true,
+          })
+          .start();
+      }
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -21,23 +58,27 @@ export default class Foli extends React.Component {
   render() {
     const { id, name, latitude, longitude } = this.props;
     return (
-      <MapView.Marker
-        ref={id}
-        coordinate={{
-          latitude,
-          longitude,
+      <MapView.Marker.Animated
+        ref={marker => {
+          this.marker = marker;
         }}
+        coordinate={this.state.coordinate}
       >
         <View
+          renderToHardwareTextureAndroid
           style={{
             alignItems: "center",
             justifyContent: "center",
+            backgroundColor: "#fff",
+            paddingVertical: 2,
+            paddingHorizontal: 6,
+            borderRadius: 10,
           }}
         >
           <Text style={{ fontSize: 14, color: "#555" }}>{name}</Text>
           <Ionicons name="md-bus" size={32} color="green" />
         </View>
-      </MapView.Marker>
+      </MapView.Marker.Animated>
     );
   }
 }
